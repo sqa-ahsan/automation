fae5bd556352b16ab87f18abe6a0f5a77394f161
sqa-ahsan <113516150+sqa-ahsan@users.noreply.github.com>
2022-09-14 16:11:31 +0500

    Add files via upload

---
+++ Nullbrainer/Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>30f316c3-9f69-46f6-9af0-ca5c94061e87</testSuiteGuid>
+   <testCaseLink>
+      <guid>9bf9d52d-f194-409b-8e56-e6036d68f4e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Nullbrainer contact us</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
